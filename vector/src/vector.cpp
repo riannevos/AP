868d10baddbf92b09eaa803b1ed654ca83b876e4
Rianne van Os <rianne.vanos@hu.nl>
2023-05-10 13:58:14 +0200

    voeg testen toe aan vector project

--- vector/src/vector.cpp
+++ vector/src/vector.cpp
@@ -1,11 +1,11 @@
 #include "include/vector.hpp"
 
-Vector::Vector(){
+Vector::Vector() {
     x = 0;
     y = 0;
 }
 
-Vector::Vector(int x, int y){
+Vector::Vector(int x, int y) {
     this->x = x;
     this->y = y;
 }
@@ -14,12 +14,11 @@ Vector Vector::operator-() const {
     return Vector(-x, -y);
 }
 
-Vector Vector::operator-(const Vector& rhs) const{
-    return Vector(x-rhs.x, y - rhs.y);
+Vector Vector::operator-(const Vector& rhs) const {
+    return Vector(x - rhs.x, y - rhs.y);
 }
 
-
-Vector& Vector::operator-=(const Vector& rhs){
+Vector &Vector::operator-=(const Vector& rhs) {
     x -= rhs.x;
     y -= rhs.y;
     return *this;
